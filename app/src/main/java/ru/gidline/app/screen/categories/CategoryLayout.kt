3f10c75aa6e04c12b8c495ef594a34485fab8ce5
vlad <vladkalyuzhnyu@gmail.com>
2020-03-03 09:25:46 +0300

    category

--- app/src/main/java/ru/gidline/app/screen/categories/CategoryLayout.kt
+++ app/src/main/java/ru/gidline/app/screen/categories/CategoryLayout.kt
@@ -5,26 +5,34 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.merge_category.view.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
 import ru.gidline.app.R
+import ru.gidline.app.extension.use
 
-class CategoryLayout : MaterialCardView, KodeinAware {
+class CategoryLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialCardView(context, attrs, defStyleAttr), KodeinAware {
 
     override val kodein by closestKodein()
 
-    @JvmOverloads
-    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
+    init {
         init(attrs)
     }
 
     @SuppressLint("Recycle")
     private fun init(attrs: AttributeSet?) {
         View.inflate(context, R.layout.merge_category, this)
+        attrs?.let { set ->
+            context.obtainStyledAttributes(set, R.styleable.CategoryLayout).use {
+                getString(R.styleable.CategoryLayout_text)?.let {
+                    tv_name.text = it
+                }
+            }
+        }
     }
 
     override fun hasOverlappingRendering() = false
