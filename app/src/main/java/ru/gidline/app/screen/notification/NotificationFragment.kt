07d6e5a5cea98b2920aa46bbfb35ea9033d23302
vlad <vladkalyuzhnyu@gmail.com>
2020-03-07 16:56:58 +0300

    notification

--- app/src/main/java/ru/gidline/app/screen/notification/NotificationFragment.kt
+++ app/src/main/java/ru/gidline/app/screen/notification/NotificationFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.fragment_notification.*
 import org.kodein.di.generic.instance
 import ru.gidline.app.R
 import ru.gidline.app.local.BellRepository
+import ru.gidline.app.local.dto.BellType
 import ru.gidline.app.screen.base.BaseFragment
+import ru.gidline.app.screen.main.MainContract
 
 class NotificationFragment : BaseFragment<NotificationContract.Presenter>(), NotificationContract.View {
 
@@ -18,16 +21,27 @@ class NotificationFragment : BaseFragment<NotificationContract.Presenter>(), Not
     private val bellRepository: BellRepository by instance()
 
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
+        bellRepository.getById(args.getInt("id"))?.let {
+            makeCallback<MainContract.View> {
+                setTitle(it.type.caption)
+            }
+        }
         return inflater.inflate(R.layout.fragment_notification, root, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val bell = bellRepository.getById(args.getInt("id"))
-        tv_title.text = bell.title
-        tv_vacancy.text = bell.vacancy
-        tv_subtext.text = bell.subtitle
-        tv_html.text = HtmlCompat.fromHtml(bell.html, HtmlCompat.FROM_HTML_MODE_LEGACY)
-        fab_phone
+        bellRepository.getById(args.getInt("id"))?.let {
+            tv_title.text = it.title
+            it.vacancy?.let { vacancy ->
+                tv_vacancy.apply {
+                    isVisible = true
+                    text = vacancy
+                }
+            }
+            tv_subtext.text = it.subtitle
+            tv_html.text = HtmlCompat.fromHtml(it.html, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            fab_phone.isVisible = it.type == BellType.INVITATION
+        }
     }
 
     companion object {
