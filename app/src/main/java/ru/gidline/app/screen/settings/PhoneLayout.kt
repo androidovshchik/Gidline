655ea10ba0f8a529d2f5a2daf15a3d892f416959
vlad <vladkalyuzhnyu@gmail.com>
2020-03-08 15:14:48 +0300

    settings

---
+++ app/src/main/java/ru/gidline/app/screen/settings/PhoneLayout.kt
@@ -0,0 +1,44 @@
+package ru.gidline.app.screen.settings
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import ru.gidline.app.R
+
+class PhoneLayout : RelativeLayout, KodeinAware {
+
+    override val kodein by closestKodein()
+
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(attrs)
+    }
+
+    @Suppress("unused")
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    ) {
+        init(attrs)
+    }
+
+    @SuppressLint("Recycle")
+    private fun init(attrs: AttributeSet?) {
+        View.inflate(context, R.layout.merge_phone, this)
+    }
+
+    override fun hasOverlappingRendering() = false
+}
