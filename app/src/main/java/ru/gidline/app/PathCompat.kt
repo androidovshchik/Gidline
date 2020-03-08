37b6a3ba0e7788f547977538502a1e80d37cb883
vlad <vladkalyuzhnyu@gmail.com>
2020-03-08 15:50:58 +0300

    path compat

---
+++ app/src/main/java/ru/gidline/app/PathCompat.kt
@@ -0,0 +1,90 @@
+@file:Suppress("FunctionName")
+
+package ru.gidline.app
+
+import android.annotation.SuppressLint
+import android.content.ContentUris
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import androidx.annotation.WorkerThread
+import ru.gidline.app.extension.isKitkatPlus
+import timber.log.Timber
+
+@Suppress("SpellCheckingInspection")
+object PathCompat {
+
+    @WorkerThread
+    fun getPath(context: Context, uri: Uri): String? = context.run {
+        return try {
+            when {
+                !isKitkatPlus() -> getDataColumn(uri, null, null)
+                else -> getPathKitkatPlus(uri)
+            }
+        } catch (e: Throwable) {
+            Timber.e(e)
+            null
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    @SuppressLint("NewApi", "DefaultLocale")
+    private fun Context.getPathKitkatPlus(uri: Uri): String? {
+        if (DocumentsContract.isDocumentUri(applicationContext, uri)) {
+            val docId = DocumentsContract.getDocumentId(uri)
+            if (uri.isExternalStorageDocument) {
+                val parts = docId.split(":")
+                if ("primary".equals(parts[0], true)) {
+                    return "${Environment.getExternalStorageDirectory()}/${parts[1]}"
+                }
+            } else if (uri.isDownloadsDocument) {
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"),
+                    docId.toLong()
+                )
+                return getDataColumn(contentUri, null, null)
+            } else if (uri.isMediaDocument) {
+                val parts = docId.split(":")
+                val contentUri = when (parts[0].toLowerCase()) {
+                    "image" -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    "video" -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    "audio" -> MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                    else -> return null
+                }
+                return getDataColumn(contentUri, "_id=?", arrayOf(parts[1]))
+            }
+        } else if ("content".equals(uri.scheme, true)) {
+            return if (uri.isGooglePhotosUri) {
+                uri.lastPathSegment
+            } else {
+                getDataColumn(uri, null, null)
+            }
+        } else if ("file".equals(uri.scheme, true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    private fun Context.getDataColumn(uri: Uri, selection: String?, args: Array<String>?): String? {
+        contentResolver?.query(uri, arrayOf("_data"), selection, args, null)?.use {
+            if (it.moveToFirst()) {
+                return it.getString(it.getColumnIndexOrThrow("_data"))
+            }
+        }
+        return null
+    }
+
+    private val Uri.isExternalStorageDocument: Boolean
+        get() = authority == "com.android.externalstorage.documents"
+
+    private val Uri.isDownloadsDocument: Boolean
+        get() = authority == "com.android.providers.downloads.documents"
+
+    private val Uri.isMediaDocument: Boolean
+        get() = authority == "com.android.providers.media.documents"
+
+    private val Uri.isGooglePhotosUri: Boolean
+        get() = authority == "com.google.android.apps.photos.content"
+}
