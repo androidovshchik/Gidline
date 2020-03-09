d416bc9f39e27e3ce936a0db29c51f6678339996
vlad <vladkalyuzhnyu@gmail.com>
2020-03-09 10:44:42 +0300

    res

--- app/src/main/java/ru/gidline/app/screen/settings/SettingsContract.kt
+++ app/src/main/java/ru/gidline/app/screen/settings/SettingsContract.kt
@@ -2,12 +2,13 @@ package ru.gidline.app.screen.settings
 
 import android.content.Context
 import android.net.Uri
+import android.widget.AdapterView
 import ru.gidline.app.screen.base.listener.IPresenter
 import ru.gidline.app.screen.base.listener.IView
 
 interface SettingsContract {
 
-    interface View : IView {
+    interface View : IView, AdapterView.OnItemSelectedListener {
 
         fun onPhotoPath(path: String?)
     }
