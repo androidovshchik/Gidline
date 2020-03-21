ab42b6d556206bdad29d547a6bffa2e2a38263fe
vlad <vladkalyuzhnyu@gmail.com>
2020-03-21 19:03:24 +0300

    filter

--- app/src/main/java/ru/gidline/app/screen/catalog/map/MapFragment.kt
+++ app/src/main/java/ru/gidline/app/screen/catalog/map/MapFragment.kt
@@ -1,22 +1,9 @@
 package ru.gidline.app.screen.catalog.map
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import org.kodein.di.generic.instance
-import ru.gidline.app.R
-import ru.gidline.app.screen.base.BaseFragment
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapFragment : BaseFragment<MapContract.Presenter>(), MapContract.View {
-
-    override val presenter: MapPresenter by instance()
-
-    override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_map, root, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
+class MapFragment : SupportMapFragment() {
 
     companion object {
 
--- app/src/main/java/ru/gidline/app/screen/catalog/map/MapPresenter.kt
+++
@@ -1,7 +0,0 @@
-package ru.gidline.app.screen.catalog.map
-
-import android.content.Context
-import ru.gidline.app.screen.base.BasePresenter
-
-class MapPresenter(context: Context) : BasePresenter<MapContract.View>(context),
-    MapContract.Presenter
