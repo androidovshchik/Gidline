22cfca1849841307a9063435be31a68de3c4e4c3
vlad <vladkalyuzhnyu@gmail.com>
2020-03-30 13:20:13 +0300

    places

--- app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesContract.kt
+++ app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesContract.kt
@@ -5,7 +5,6 @@ import ru.gidline.app.screen.base.listener.IPresenter
 import ru.gidline.app.screen.base.listener.IRecycler
 import ru.gidline.app.screen.base.listener.IView
 import ru.gidline.app.screen.catalog.CatalogContract
-import ru.gidline.app.screen.catalog.CatalogFilter
 
 interface PlacesContract {
 
@@ -13,8 +12,5 @@ interface PlacesContract {
 
     interface Recycler : IRecycler<Place>
 
-    interface Presenter : IPresenter<View> {
-
-        fun applyDistances(catalogFilter: CatalogFilter)
-    }
+    interface Presenter : IPresenter<View>
 }
--- app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesFragment.kt
+++ app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesFragment.kt
@@ -4,20 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.fragment_places.*
 import org.kodein.di.generic.instance
 import ru.gidline.app.R
+import ru.gidline.app.local.model.Place
 import ru.gidline.app.screen.base.BaseFragment
+import ru.gidline.app.screen.catalog.CatalogContract
+import ru.gidline.app.screen.catalog.CatalogFilter
 
 @Suppress("MemberVisibilityCanBePrivate")
 class PlacesFragment : BaseFragment<PlacesContract.Presenter>(), PlacesContract.View {
 
     override val presenter: PlacesPresenter by instance()
 
+    private var hasUpdatedList = false
+
+    private val catalogFilter: CatalogFilter?
+        get() {
+            parentCallback<CatalogContract.View>(true) {
+                return catalogFilter
+            }
+            return null
+        }
+
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
         return inflater.inflate(R.layout.fragment_places, root, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+
+    override fun onFilterUpdate() {
+        catalogFilter?.let {
+            pl_consulate.isVisible = it.typeId == R.id.ib_all || it.typeId == R.id.ib_consulate
+            pl_migration.isVisible = it.typeId == R.id.ib_all || it.typeId == R.id.ib_migration
+        }
+    }
+
+    override fun onNewLocation() {
+        if (!hasUpdatedList) {
+            hasUpdatedList = true
+            pl_consulate.updateData()
+            pl_migration.updateData()
+        }
+    }
+
+    override fun onItemSelected(position: Int, item: Place) {
 
     }
 
