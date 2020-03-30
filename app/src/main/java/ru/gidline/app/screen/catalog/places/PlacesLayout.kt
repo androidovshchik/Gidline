af785bf3ed76abf17cb3c23042bfa66f4f2c24a3
vlad <vladkalyuzhnyu@gmail.com>
2020-03-30 13:05:51 +0300

    placelayout

--- app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesLayout.kt
+++ app/src/main/java/ru/gidline/app/screen/catalog/places/PlacesLayout.kt
@@ -2,11 +2,13 @@ package ru.gidline.app.screen.catalog.places
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.view.isVisible
 import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.merge_places.view.*
+import org.jetbrains.anko.dip
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
 import org.kodein.di.generic.instance
@@ -31,18 +33,22 @@ class PlacesLayout @JvmOverloads constructor(
 
     private val adapter = PlacesAdapter(this)
 
+    private var type: String? = null
+
     init {
         init(attrs)
     }
 
     @SuppressLint("Recycle", "SetTextI18n")
     private fun init(attrs: AttributeSet?) {
+        radius = dip(10).toFloat()
+        setCardBackgroundColor(Color.parseColor("#5cffffff"))
         View.inflate(context, R.layout.merge_places, this)
         attrs?.let { set ->
             context.obtainStyledAttributes(set, R.styleable.PlacesLayout).use {
                 getString(R.styleable.PlacesLayout_text)?.let {
                     tv_name.text = it
-                    val type = when (it) {
+                    type = when (it) {
                         resources.getString(R.string.places_consulate) -> Place.CONSULATE
                         resources.getString(R.string.places_migration) -> Place.MIGRATION
                         else -> null
@@ -58,18 +64,12 @@ class PlacesLayout @JvmOverloads constructor(
                 }
             }
         }
-        if (adapter.items.size > 3) {
-            updateToggle()
-            c_toggle.isVisible = true
-            c_toggle.setOnClickListener {
-                context.activityCallback<IView> {
-                    adapter.apply {
-                        limited = !limited
-                        notifyDataSetChanged()
-                    }
-                    updateToggle()
-                }
+        c_toggle.setOnClickListener {
+            adapter.apply {
+                limited = !limited
+                notifyDataSetChanged()
             }
+            updateToggle()
         }
         rv_places.also {
             it.isNestedScrollingEnabled = false
@@ -88,8 +88,22 @@ class PlacesLayout @JvmOverloads constructor(
         }
     }
 
+    fun updateData() {
+        adapter.apply {
+            items.clear()
+            items.addAll(placeRepository.getByType(type))
+            notifyDataSetChanged()
+            if (adapter.items.size > 3) {
+                updateToggle()
+            }
+        }
+    }
+
     private fun updateToggle() {
-        c_toggle.text = if (adapter.limited) "смотреть все ${adapter.items.size}" else "СВЕРНУТЬ"
+        c_toggle.apply {
+            isVisible = true
+            text = if (adapter.limited) "смотреть все ${adapter.items.size}" else "СВЕРНУТЬ"
+        }
     }
 
     override fun hasOverlappingRendering() = false
