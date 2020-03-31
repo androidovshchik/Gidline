39312caf0817adb327d5569050d8e3a19fc96ff2
vlad <vladkalyuzhnyu@gmail.com>
2020-03-31 11:24:03 +0300

    cluster view wins

--- app/src/main/java/ru/gidline/app/screen/catalog/map/ClusterView.kt
+++ app/src/main/java/ru/gidline/app/screen/catalog/map/ClusterView.kt
@@ -7,7 +7,9 @@ import android.graphics.Color
 import android.os.Build
 import android.util.AttributeSet
 import android.widget.TextView
+import org.jetbrains.anko.dip
 import ru.gidline.app.R
+import kotlin.math.max
 
 @SuppressLint("AppCompatCustomView")
 class ClusterView : TextView {
@@ -34,9 +36,17 @@ class ClusterView : TextView {
 
     @SuppressLint("Recycle")
     private fun init(attrs: AttributeSet?) {
+        val padding = dip(10)
         id = R.id.amu_text
         setTextColor(Color.parseColor("#d18bc9"))
         setBackgroundResource(R.drawable.background_consulate)
+        setPadding(padding, padding, padding, padding)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val size = max(measuredWidth, measuredHeight)
+        setMeasuredDimension(size, size)
     }
 
     override fun hasOverlappingRendering() = false
