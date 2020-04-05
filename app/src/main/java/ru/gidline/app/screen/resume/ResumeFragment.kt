3dee0dc3a8e0a11faedf10defbf5307f8e8957d0
vlad <vladkalyuzhnyu@gmail.com>
2020-04-05 08:32:20 +0300

    resume

---
+++ app/src/main/java/ru/gidline/app/screen/resume/ResumeFragment.kt
@@ -0,0 +1,31 @@
+package ru.gidline.app.screen.resume
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.kodein.di.generic.instance
+import ru.gidline.app.R
+import ru.gidline.app.screen.base.BaseFragment
+
+class ResumeFragment : BaseFragment<ResumeContract.Presenter>(), ResumeContract.View {
+
+    override val presenter: ResumePresenter by instance()
+
+    override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_settings, root, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+
+    companion object {
+
+        fun newInstance(): ResumeFragment {
+            return ResumeFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ru/gidline/app/screen/resume/ResumePresenter.kt
@@ -0,0 +1,7 @@
+package ru.gidline.app.screen.resume
+
+import android.content.Context
+import ru.gidline.app.screen.base.BasePresenter
+
+class ResumePresenter(context: Context) : BasePresenter<ResumeContract.View>(context),
+    ResumeContract.Presenter
