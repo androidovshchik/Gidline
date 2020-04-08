664aca41f61fa0243925d4df936e4126fee97562
vlad <vladkalyuzhnyu@gmail.com>
2020-04-08 14:40:03 +0300

    shape

---
+++ app/src/main/java/ru/gidline/app/screen/common/shape/ShapeFrameLayout.kt
@@ -0,0 +1,32 @@
+package ru.gidline.app.screen.common.shape
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class ShapeFrameLayout : FrameLayout, ShapeViewGroup {
+
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(attrs)
+    }
+
+    @Suppress("unused")
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    ) {
+        init(attrs)
+    }
+
+    override fun hasOverlappingRendering() = false
+}
---
+++ app/src/main/java/ru/gidline/app/screen/common/shape/ShapeViewGroup.kt
@@ -0,0 +1,13 @@
+package ru.gidline.app.screen.common.shape
+
+import android.annotation.SuppressLint
+import android.util.AttributeSet
+
+interface ShapeViewGroup {
+
+    @Suppress("UNUSED_PARAMETER")
+    @SuppressLint("Recycle", "SetTextI18n")
+    fun init(attrs: AttributeSet?) {
+
+    }
+}
